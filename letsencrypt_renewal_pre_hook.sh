bdd343ae2141bb9f9962b8d26195fa5e9dddda45
hat3ph <88069788+hat3ph@users.noreply.github.com>
2022-03-27 06:42:54 +0800

    Delete letsencrypt_renewal_pre_hook.sh

--- letsencrypt_renewal_pre_hook.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# stop adguard-unbound container
-# change the path to where you keep the docker-compose.yml
-docker-compose -f /home/ubuntu/docker-adguard-unbound/docker-compose.yml down
